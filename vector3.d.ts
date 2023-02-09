0002ace4198033f47fa1e71b9c9e4eebe9fb6f4f
3Dlyra <3dlyra.com@gmail.com>
2023-02-10 04:45:04 +0500

    Implement `create` method

---
+++ vector3.d.ts
@@ -0,0 +1,1 @@
+export function create( x?: number, y?: number, z?: number ): Float32Array;
---
+++ vector3.js
@@ -0,0 +1,4 @@
+export function create( x, y, z ) {
+
+	return new Float32Array( [ x || 0, y || 0, z || 0 ] )
+}
