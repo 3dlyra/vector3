f5ffe46024864631e2e74940d59eed8722eadfc2
3Dlyra <3dlyra.com@gmail.com>
2023-02-10 13:51:15 +0500

    Implement `add` method

--- vector3.d.ts
+++ vector3.d.ts
@@ -1,1 +1,2 @@
 export function create( x?: number, y?: number, z?: number ): Float32Array;
+export function add( a: Float32Array, b: Float32Array ): Float32Array;
--- vector3.js
+++ vector3.js
@@ -2,3 +2,12 @@ export function create( x, y, z ) {
 
 	return new Float32Array( [ x || 0, y || 0, z || 0 ] )
 }
+
+export function add( a, b ) {
+
+	return new Float32Array( [
+		a[ 0 ] + b[ 0 ],
+		a[ 1 ] + b[ 1 ],
+		a[ 2 ] + b[ 2 ]
+	] )
+}
